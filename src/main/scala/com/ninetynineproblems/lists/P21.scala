99b5a6b60b687c585eb0ac56d2d8bd157b7ba442
devang7 <dnjhaver@usc.edu>
2019-03-03 23:28:53 -0800

    Solved P21, P22

--- src/main/scala/com/ninetynineproblems/lists/P21.scala
+++ src/main/scala/com/ninetynineproblems/lists/P21.scala
@@ -7,3 +7,20 @@ Example:
 scala> insertAt('new, 1, List('a, 'b, 'c, 'd))
 res0: List[Symbol] = List('a, 'new, 'b, 'c, 'd)
 */
+class P21 {
+  def insertAt[T](key: T, k: Int, x: List[T]): List[T] = {
+    if (k < 0) throw new IllegalArgumentException
+
+    x match {
+      case head :: tail => {
+        if (k == 0) {
+          key :: x
+        } else {
+          head :: insertAt(key, k - 1, tail)
+        }
+      }
+      case List() => if (k == 0) List(key) else throw new NoSuchElementException
+      case _ => throw new NoSuchElementException
+    }
+  }
+}
--- src/main/scala/com/ninetynineproblems/lists/P22.scala
+++ src/main/scala/com/ninetynineproblems/lists/P22.scala
@@ -5,3 +5,8 @@ Example:
 scala> range(4, 9)
 res0: List[Int] = List(4, 5, 6, 7, 8, 9)
 */
+class P22 {
+  def range(start: Int, end: Int): List[Int] = {
+    if (start > end) Nil else start :: range(start + 1, end)
+  }
+}
